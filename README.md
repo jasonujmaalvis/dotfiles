0287fc90d9a05c93b95d1fed0594c487c31c2b43
Jason Ujma-Alvis <alvis.jason@gmail.com>
2019-03-14 12:06:41 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dotfiles
+
+These are my config settings for various apps, editors and linters. Feel free to use them all or pick ones that might make sense for you.
---
+++ vscode.js
@@ -0,0 +1,66 @@
+{
+  "window.title": "${dirty} ${activeEditorMedium}${separator}${rootName}",
+  "workbench.colorTheme": "Night Owl",
+  "workbench.editor.tabSizing": "shrink",
+  "workbench.startupEditor": "newUntitledFile",
+  "workbench.editor.enablePreviewFromQuickOpen": false,
+  "workbench.iconTheme": "ayu",
+  "explorer.autoReveal": false,
+  "editor.fontFamily": "Dank Mono",
+  "editor.fontLigatures": true,
+  "editor.fontSize": 18,
+  "editor.lineHeight": 25,
+  "editor.letterSpacing": 0.5,
+  "editor.tabCompletion": true,
+  "editor.cursorStyle": "line",
+  "editor.cursorWidth": 5,
+  "editor.cursorBlinking": "solid",
+  "editor.formatOnSave": true,
+  "editor.formatOnPaste": true,
+  "editor.tabSize": 2,
+  "editor.autoIndent": true,
+  "editor.detectIndentation": false,
+  "editor.matchBrackets": true,
+  "editor.renderWhitespace": "none",
+  "editor.renderControlCharacters": false,
+  "editor.minimap.enabled": false,
+  "editor.snippetSuggestions": "top",
+  "[javascript]": {
+    "editor.formatOnSave": false
+  },
+  "eslint.autoFixOnSave": true,
+  "eslint.alwaysShowStatus": true,
+  "prettier.disableLanguages": ["js"],
+  "bracketPairColorizer.forceIterationColorCycle": true,
+  "search.exclude": {
+    "**/node_modules": true,
+    "**/bower_components": true,
+    "**/.nuxt": true,
+    "/bin": true,
+    "/dev": true,
+    "/pub": true,
+    "/setup": true,
+    "/var": true
+  },
+  "files.exclude": {
+    "**/.git": true,
+    "**/.svn": true,
+    "**/.hg": true,
+    "**/CVS": true,
+    "**/.DS_Store": true,
+    "**/.next": true
+  },
+  "files.trimTrailingWhitespace": true,
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact",
+    "vue-html": "html"
+  },
+  "emmet.syntaxProfiles": {
+    "javascript": "jsx"
+  },
+  "vscode_custom_css.imports": ["file:///Users/jasonalvis/.vscodestyles.css"],
+  "files.associations": {
+    "*.css": "postcss",
+    "*.phtml": "php"
+  }
+}
